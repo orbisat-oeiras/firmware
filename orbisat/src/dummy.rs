2c64194ba4974fef626177e793f9e9ae6c2d915a
Leviathan <levi.mcgomes@gmail.com>
2025-04-18 11:44:35 +0100

    proto: change DummySender to use the new TmPacketSender

--- orbisat/src/dummy.rs
+++ orbisat/src/dummy.rs
@@ -1,30 +1,29 @@
 use std::time::Duration;
 
-use orbipacket::{DeviceId, Packet, Payload, Timestamp, TmPacket};
+use orbipacket::{DeviceId, Packet, Payload};
+use tokio::sync::mpsc::Sender;
 
-use crate::{cancellable_loop, signal::SmartSignal};
+use crate::{cancellable_loop, signal::SmartSignal, tmtc::TmPacketSender};
 
 pub struct DummySender {
-    pub(crate) send: tokio::sync::mpsc::Sender<Packet>,
+    packet_sender: TmPacketSender,
 }
 
 impl DummySender {
-    pub fn new(send: tokio::sync::mpsc::Sender<Packet>) -> Self {
-        Self { send }
+    pub fn new(send: Sender<Packet>) -> Self {
+        Self {
+            packet_sender: TmPacketSender::new(send, DeviceId::MissingDevice),
+        }
     }
 
     pub async fn steady(&mut self, cancel: SmartSignal) -> anyhow::Result<()> {
         let mut interval = tokio::time::interval(Duration::from_millis(500));
         cancellable_loop!(cancel => {
             loop {
-                let packet = TmPacket::new(
-                    DeviceId::MissingDevice,
-                    Timestamp::new(10),
-                    Payload::from_bytes(&[11])?,
-                );
+                let packet = Payload::from_bytes(&[11])?;
 
                 println!("Send packet");
-                self.send.send(Packet::TmPacket(packet)).await?;
+                self.packet_sender.send(packet).await?;
                 interval.tick().await;
             }
         })
