c0f8e256449340610a133ea69a42de053c241b87
Leviathan <levi.mcgomes@gmail.com>
2025-04-30 18:12:41 +0000

    feat: implement new features in main

--- orbisat/src/main.rs
+++ orbisat/src/main.rs
@@ -1,18 +1,38 @@
-use orbisat::{dummy::DummySender, signal::SmartSignal, tmtc::SerialPacketSink};
-use rppal::uart;
+use embedded_hal_bus::{i2c::AtomicDevice, util::AtomicCell};
+use orbisat::{
+    sensor::PressureTemperatureHumiditySensor, signal::SmartSignal, store::FileStore,
+    tmtc::SerialPacketSink,
+};
+use rppal::{hal, i2c, uart};
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let signal = SmartSignal::new()?;
 
-    let uart = uart::Uart::new(19200, uart::Parity::None, 8, 1)?;
-    let (tx, rx) = tokio::sync::mpsc::channel(100);
+    //let uart = uart::Uart::new(19200, uart::Parity::None, 8, 1)?;
+    let uart = uart::Uart::with_path("/dev/ttyUSB0", 19200, uart::Parity::None, 8, 1)?;
+    let (tx, rx) = tokio::sync::broadcast::channel(100);
 
     let mut sink = SerialPacketSink::new(uart, rx);
 
-    let mut sender = DummySender::new(tx);
+    let mut store = FileStore::new(tx.subscribe())?;
 
-    tokio::try_join!(sink.steady(signal.clone()), sender.steady(signal.clone()))?;
+    let i2c = i2c::I2c::new()?;
+    let i2c = AtomicCell::new(i2c);
+
+    let delay = hal::Delay::new();
+
+    let sensor = PressureTemperatureHumiditySensor::new(AtomicDevice::new(&i2c), delay, tx.clone());
+    let mut sensor = match sensor {
+        Ok(sensor) => sensor,
+        Err(err) => anyhow::bail!(format!("{:?}", err)),
+    };
+
+    tokio::try_join!(
+        sink.steady(signal.clone()),
+        sensor.steady(signal.clone()),
+        store.steady(signal.clone()),
+    )?;
 
     Ok(())
 }
