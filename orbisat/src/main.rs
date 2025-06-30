8c00c73e8d6ba785c4548a1bb033791df1104436
Leviathan <levi.mcgomes@gmail.com>
2025-06-30 17:31:30 +0100

    feat: update main program

--- orbisat/src/main.rs
+++ orbisat/src/main.rs
@@ -1,6 +1,11 @@
 use embedded_hal_bus::{i2c::AtomicDevice, util::AtomicCell};
 use orbisat::{
-    sensor::PressureTemperatureHumiditySensor, signal::SmartSignal, store::FileStore,
+    logger::ConsoleLogger,
+    navigation::Gnss,
+    sensor::PressureTemperatureHumiditySensor,
+    signal::SmartSignal,
+    store::FileStore,
+    system::{AltitudeMonitor, HeartbeatSender},
     tmtc::SerialPacketSink,
 };
 use rppal::{hal, i2c, uart};
@@ -9,13 +14,16 @@ use rppal::{hal, i2c, uart};
 async fn main() -> anyhow::Result<()> {
     let signal = SmartSignal::new()?;
 
-    //let uart = uart::Uart::new(19200, uart::Parity::None, 8, 1)?;
     let uart = uart::Uart::with_path("/dev/ttyUSB0", 19200, uart::Parity::None, 8, 1)?;
     let (tx, rx) = tokio::sync::broadcast::channel(100);
 
     let mut sink = SerialPacketSink::new(uart, rx);
 
     let mut store = FileStore::new(tx.subscribe())?;
+
+    let mut logger = ConsoleLogger::new(tx.subscribe());
+
+    let mut heartbeat = HeartbeatSender::new(tx.clone());
 
     let i2c = i2c::I2c::new()?;
     let i2c = AtomicCell::new(i2c);
@@ -28,10 +36,19 @@ async fn main() -> anyhow::Result<()> {
         Err(err) => anyhow::bail!(format!("{:?}", err)),
     };
 
+    let uart = uart::Uart::new(9600, uart::Parity::None, 8, 1)?;
+    let mut gnss = Gnss::new(uart, tx.clone());
+
+    let mut monitor = AltitudeMonitor::new(tx.subscribe(), tx.clone());
+
     tokio::try_join!(
+        heartbeat.steady(signal.clone()),
         sink.steady(signal.clone()),
-        sensor.steady(signal.clone()),
         store.steady(signal.clone()),
+        logger.steady(signal.clone()),
+        // monitor.steady(signal.clone()),
+        sensor.steady(signal.clone()),
+        gnss.steady(signal.clone()),
     )?;
 
     Ok(())
