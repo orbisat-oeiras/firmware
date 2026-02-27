2b642bcc94d6e058feef2548a53397fc40e76243
Leviathan <levi.mcgomes@gmail.com>
2026-02-27 14:11:39 +0000

    feat(components): add ConsolePacketSink

--- orbisat-components/src/lib.rs
+++ orbisat-components/src/lib.rs
@@ -1,1 +1,11 @@
 #![no_std]
+
+use orbisat::PacketSink;
+
+pub struct ConsolePacketSink;
+
+impl PacketSink for ConsolePacketSink {
+    async fn sink_bytes(&self, buf: &[u8]) {
+        defmt::info!("Packet received: {}", buf);
+    }
+}
