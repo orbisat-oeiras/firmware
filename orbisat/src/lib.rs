164d5583f7fb325955cf1481abbb54e204f2fa46
Leviathan <levi.mcgomes@gmail.com>
2026-02-27 14:09:16 +0000

    feat(orbisat): add Component trait

--- orbisat/src/lib.rs
+++ orbisat/src/lib.rs
@@ -1,1 +1,11 @@
 #![no_std]
+
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Receiver};
+
+pub trait Component {
+    fn run(
+        &self,
+        recv: Receiver<'static, CriticalSectionRawMutex, Packet, 256>,
+    ) -> impl core::future::Future<Output = ()> + Send;
+}
+
