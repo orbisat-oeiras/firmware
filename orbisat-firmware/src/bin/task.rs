5446d02ae6db922939c6d4d3247008cef329e88f
Leviathan <levi.mcgomes@gmail.com>
2026-03-20 10:04:22 +0000

    feat(firmware): add sequential binary and test SerialByteSink

---
+++ orbisat-firmware/src/bin/task.rs
@@ -0,0 +1,76 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use embassy_time::Timer;
+use esp_hal::Async;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::uart::Config;
+use esp_hal::{clock::CpuClock, uart::Uart};
+use orbipacket::{DeviceId, Packet, Payload, Timestamp, TmPacket};
+use orbisat::{Component, comms::PacketSink};
+use orbisat_components::{ConsoleByteSink, SerialByteSink};
+use {esp_backtrace as _, esp_println as _};
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+static CHANNEL: Channel<CriticalSectionRawMutex, Packet, 1> = Channel::new();
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.0.1
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+
+    info!("Embassy initialized!");
+
+    let console_sink = PacketSink::new(CHANNEL.receiver(), ConsoleByteSink);
+    spawner.spawn(console_sink_task(console_sink)).unwrap();
+
+    let serial_sink = PacketSink::new(
+        CHANNEL.receiver(),
+        SerialByteSink::new(
+            Uart::new(peripherals.UART2, Config::default())
+                .unwrap()
+                .with_rx(peripherals.GPIO12)
+                .with_tx(peripherals.GPIO13)
+                .into_async(),
+        ),
+    );
+    spawner.spawn(serial_sink_task(serial_sink)).unwrap();
+
+    loop {
+        CHANNEL
+            .sender()
+            .send(Packet::TmPacket(TmPacket::new(
+                DeviceId::System,
+                Timestamp::new(10).unwrap(),
+                Payload::from_u8(10),
+            )))
+            .await;
+        Timer::after_millis(1000).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn console_sink_task(mut sink: PacketSink<ConsoleByteSink>) {
+    sink.run().await;
+}
+
+#[embassy_executor::task]
+async fn serial_sink_task(mut sink: PacketSink<SerialByteSink<Uart<'static, Async>>>) {
+    sink.run().await;
+}
