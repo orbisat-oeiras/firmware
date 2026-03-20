64792cb4896e8d08c7d241abca8e164359c26d62
Leviathan <levi.mcgomes@gmail.com>
2026-03-20 10:01:52 +0000

    feat(components): add SerialByteSink

--- orbisat-components/src/lib.rs
+++ orbisat-components/src/lib.rs
@@ -9,3 +9,27 @@ impl ByteSink for ConsoleByteSink {
         defmt::info!("Packet received: {}", buf);
     }
 }
+
+pub struct SerialByteSink<W>(W)
+where
+    W: embedded_io_async::Write;
+
+impl<W> SerialByteSink<W>
+where
+    W: embedded_io_async::Write,
+{
+    pub fn new(write: W) -> Self {
+        Self(write)
+    }
+}
+
+impl<W> ByteSink for SerialByteSink<W>
+where
+    W: embedded_io_async::Write,
+{
+    async fn sink(&mut self, buf: &[u8]) {
+        // TODO: proper error handling
+        self.0.write_all(buf).await.unwrap();
+        self.0.flush().await.unwrap();
+    }
+}
