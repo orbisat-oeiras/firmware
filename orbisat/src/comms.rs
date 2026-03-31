267e14d85f412bdd9d2f3d2eb6df5e8ed291e946
Leviathan <levi.mcgomes@gmail.com>
2026-03-31 11:20:29 +0100

    feat(orbisat): add PacketSource component

--- orbisat/src/comms.rs
+++ orbisat/src/comms.rs
@@ -1,6 +1,6 @@
 use embassy_sync::pubsub::WaitResult;
-use orbipacket::{DeviceId, Packet};
-use orbisat_firmware_config::packet_channel::PacketChannelSubscriber;
+use orbipacket::{DeviceId, Packet, Payload, Timestamp, TmPacket};
+use orbisat_firmware_config::packet_channel::{PacketChannelPublisher, PacketChannelSubscriber};
 
 use crate::Component;
 
@@ -48,5 +48,72 @@ where
         };
         let packet = packet.encode(&mut self.buf).unwrap();
         self.sink.sink(packet).await;
+    }
+}
+
+pub trait ByteSource {
+    fn fill(&mut self, buf: &mut [u8]) -> impl Future<Output = usize>;
+}
+
+pub struct PacketSource<S>
+where
+    S: ByteSource,
+{
+    send: PacketChannelPublisher<'static>,
+    source: S,
+    buf: [u8; 32],
+    buf_index: usize,
+    packet_buf: [Packet; 16],
+}
+
+impl<S> PacketSource<S>
+where
+    S: ByteSource,
+{
+    pub fn new(send: PacketChannelPublisher<'static>, source: S) -> Self {
+        Self {
+            send,
+            source,
+            buf: [0; _],
+            buf_index: 0,
+            packet_buf: [Packet::TmPacket(TmPacket::new(
+                DeviceId::System,
+                Timestamp::new(0).unwrap(),
+                Payload::new(),
+            )); _],
+        }
+    }
+}
+
+impl<S> Component for PacketSource<S>
+where
+    S: ByteSource,
+{
+    fn id(&self) -> DeviceId {
+        DeviceId::System
+    }
+
+    async fn run_once(&mut self) {
+        let filled = self.source.fill(&mut self.buf[self.buf_index..]).await;
+
+        let (remaining, packets) = match Packet::decode_stateless(
+            &mut self.buf[..self.buf_index + filled],
+            &mut self.packet_buf,
+        ) {
+            Ok(r) => r,
+            Err(e) => {
+                defmt::error!("{}", e);
+                panic!()
+            }
+        };
+
+        let idx =
+            (remaining.as_ptr() as usize - self.buf.as_ptr() as usize) / core::mem::size_of::<u8>();
+        self.buf.rotate_left(idx);
+        self.buf_index += filled - idx;
+
+        for packet in packets {
+            self.send.publish(*packet).await;
+        }
     }
 }
