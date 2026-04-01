61c8754ab038199978d93af43cbdbe5cb4e60512
Leviathan <levi.mcgomes@gmail.com>
2026-04-01 14:21:58 +0100

    feat(components): add TimeSyncComponent

--- orbisat-components/src/lib.rs
+++ orbisat-components/src/lib.rs
@@ -1,6 +1,11 @@
 #![no_std]
 
-use orbisat::comms::{ByteSink, ByteSource};
+use embassy_time::Instant;
+use orbipacket::{DeviceId, Packet, Payload, Timestamp, TmPacket};
+use orbisat::{
+    Component,
+    comms::{ByteSink, ByteSource, CommunicationError},
+};
 
 #[derive(Debug)]
 pub struct ConsoleByteSink;
@@ -58,5 +63,73 @@ where
     async fn fill(&mut self, buf: &mut [u8]) -> usize {
         // TODO: error handling
         self.0.read(buf).await.unwrap()
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum TimeSyncError {
+    #[error(transparent)]
+    Communication(#[from] CommunicationError),
+    #[error("time sync telecommand payload has an invalid length ({0})")]
+    BadRequest(usize),
+}
+
+#[derive(Debug)]
+pub struct TimeSyncComponent;
+
+impl TimeSyncComponent {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Default for TimeSyncComponent {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Component for TimeSyncComponent {
+    type Error = TimeSyncError;
+
+    fn id(&self) -> DeviceId {
+        DeviceId::TimeSync
+    }
+
+    async fn run_once(&mut self, _ctx: &mut orbisat::ContextHandle<'_>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    async fn handle_tc(
+        &mut self,
+        ctx: &mut orbisat::ContextHandle<'_>,
+        tc: orbipacket::TcPacket,
+    ) -> Result<(), Self::Error> {
+        let t1 = Instant::now().as_micros();
+        let payload = tc.payload().as_bytes();
+
+        if payload.len() != 8 {
+            return Err(TimeSyncError::BadRequest(payload.len()));
+        }
+
+        let t0 = u64::from_le_bytes([
+            payload[0], payload[1], payload[2], payload[3], payload[4], payload[5], payload[6],
+            payload[7],
+        ]);
+        let t2 = Instant::now().as_micros();
+
+        let mut payload = [0u8; 3 * 8];
+        payload[..8].copy_from_slice(&t0.to_le_bytes());
+        payload[8..16].copy_from_slice(&t1.to_le_bytes());
+        payload[16..24].copy_from_slice(&t2.to_le_bytes());
+
+        ctx.send_outbound(Packet::TmPacket(TmPacket::new(
+            DeviceId::TimeSync,
+            Timestamp::new(10).unwrap(),
+            Payload::from_raw_bytes(payload).unwrap(),
+        )))
+        .await;
+
+        Ok(())
     }
 }
