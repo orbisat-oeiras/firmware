b8f98849efcce94c8899b1faef598bf63aac7bec
Leviathan <levi.mcgomes@gmail.com>
2026-04-01 14:38:48 +0100

    feat(firmware): spawn TimeSyncComponent

--- orbisat-firmware/src/bin/task.rs
+++ orbisat-firmware/src/bin/task.rs
@@ -8,7 +8,7 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Instant, Ticker};
+use embassy_time::{Duration, Ticker};
 use esp_hal::Async;
 use esp_hal::timer::timg::TimerGroup;
 use esp_hal::uart::{Config, UartRx, UartTx};
@@ -17,7 +17,7 @@ use orbipacket::{DeviceId, Packet, Payload, Timestamp, TmPacket};
 use orbisat::comms::PacketSource;
 use orbisat::{Component, comms::PacketSink};
 use orbisat::{Context, ContextHandle};
-use orbisat_components::{ConsoleByteSink, SerialByteSink, SerialByteSource};
+use orbisat_components::{ConsoleByteSink, SerialByteSink, SerialByteSource, TimeSyncComponent};
 use orbisat_firmware::components;
 use orbisat_firmware_config::packet_channel::{InboundPacketChannel, OutboundPacketChannel};
 use static_cell::StaticCell;
@@ -67,12 +67,11 @@ async fn main(spawner: Spawner) -> ! {
                 ctx.inbound().publisher().unwrap(),
                 SerialByteSource::new(uart_rx),
             );
+            time_sync: TimeSyncComponent = ();
         }
     }
 
-    spawner
-        .spawn(inbound_packet_task(ctx.to_handle().unwrap()))
-        .unwrap();
+    info!("Components initialized");
 
     loop {
         ctx_handle
@@ -85,41 +84,5 @@ async fn main(spawner: Spawner) -> ! {
 
         counter += 1;
         tick.next().await;
-    }
-}
-
-#[embassy_executor::task]
-async fn inbound_packet_task(mut ctx: ContextHandle<'static>) {
-    loop {
-        match ctx.receive_inbound().await {
-            embassy_sync::pubsub::WaitResult::Lagged(_) => {}
-            embassy_sync::pubsub::WaitResult::Message(packet) => match packet {
-                Packet::TmPacket(_) => continue,
-                Packet::TcPacket(packet) => {
-                    if *packet.device_id() == DeviceId::TimeSync {
-                        let t1 = Instant::now().as_micros();
-                        let payload = packet.payload().as_bytes();
-
-                        defmt::warn!("payload = {:02X}", payload);
-
-                        let t0 = u64::from_le_bytes([
-                            payload[0], payload[1], payload[2], payload[3], payload[4], payload[5],
-                            payload[6], payload[7],
-                        ]);
-                        let t2 = Instant::now().as_micros();
-
-                        let payload = [t0.to_le_bytes(), t1.to_le_bytes(), t2.to_le_bytes()];
-                        let payload: [u8; 3 * 8] = unsafe { core::mem::transmute(payload) };
-
-                        ctx.send_outbound(Packet::TmPacket(TmPacket::new(
-                            DeviceId::TimeSync,
-                            Timestamp::new(10).unwrap(),
-                            Payload::from_raw_bytes(payload).unwrap(),
-                        )))
-                        .await;
-                    }
-                }
-            },
-        }
     }
 }
