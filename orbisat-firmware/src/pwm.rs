f7475bb581f58ec447e86b7ca31201d61ceeda97
Leviathan <levi.mcgomes@gmail.com>
2026-04-11 11:27:28 +0100

    feat(firmware): implement proper PWM frequency control

--- orbisat-firmware/src/pwm.rs
+++ orbisat-firmware/src/pwm.rs
@@ -1,41 +1,45 @@
-use embedded_hal::pwm::{ErrorType, SetDutyCycle};
+use embassy_time::Timer;
+#[cfg(feature = "esp32")]
+use esp_hal::peripherals::GPIO33;
 use esp_hal::{
     gpio::DriveMode,
     ledc::{
         LSGlobalClkSource, Ledc, LowSpeed,
-        channel::{
-            self, Channel, ChannelIFace, Number as ChannelNumber, config::Config as ChannelConfig,
-        },
+        channel::{ChannelIFace, Number as ChannelNumber, config::Config as ChannelConfig},
         timer::{
-            LSClockSource, Number as TimerNumber, Timer, TimerHW, TimerIFace,
-            config::{Config as LedcTimerConfig, Duty},
+            LSClockSource, Number as TimerNumber, Timer as LedcTimer, TimerHW, TimerIFace,
+            config::{Config as TimerConfig, Duty},
         },
     },
-    peripherals::{GPIO33, LEDC},
+    peripherals::LEDC,
     time::Rate,
 };
 use orbisat_components::secondary::SetFrequency;
 use static_cell::StaticCell;
 
+#[cfg(feature = "esp32")]
+type PwmPin = GPIO33<'static>;
+#[cfg(feature = "esp32s3")]
+type PwmPin = GPIO34<'static>;
+
 pub struct PwmController {
-    _ledc: &'static mut Ledc<'static>,
-    lstimer0: &'static Timer<'static, LowSpeed>,
-    channel0: &'static mut Channel<'static, LowSpeed>,
+    ledc: &'static mut Ledc<'static>,
+    lstimer0: &'static LedcTimer<'static, LowSpeed>,
+    pin: PwmPin,
+    duty: Duty,
 }
 
 impl PwmController {
-    #[cfg(feature = "esp32")]
-    pub fn new(ledc_peripheral: LEDC<'static>, gpio_peripheral: GPIO33<'static>) -> Self {
+    pub fn new(ledc_peripheral: LEDC<'static>, pin: PwmPin, duty: Duty) -> Self {
         static LEDC: StaticCell<Ledc<'static>> = StaticCell::new();
-        static LSTIMER0: StaticCell<Timer<'static, LowSpeed>> = StaticCell::new();
-        static CHANNEL0: StaticCell<Channel<'static, LowSpeed>> = StaticCell::new();
+        static LSTIMER0: StaticCell<LedcTimer<'static, LowSpeed>> = StaticCell::new();
 
         let ledc = LEDC.init(Ledc::new(ledc_peripheral));
         ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
 
         let lstimer0 = LSTIMER0.init(ledc.timer::<LowSpeed>(TimerNumber::Timer0));
         lstimer0
-            .configure(LedcTimerConfig {
+            .configure(TimerConfig {
                 duty: Duty::Duty1Bit,
                 clock_source: LSClockSource::APBClk,
                 frequency: Rate::from_hz(1000),
@@ -43,56 +47,12 @@ impl PwmController {
             .expect("should be able to configure ledc timer");
 
         let lstimer0 = &(*lstimer0);
-
-        let channel0 = CHANNEL0.init(ledc.channel(ChannelNumber::Channel0, gpio_peripheral));
-        channel0
-            .configure(ChannelConfig {
-                timer: lstimer0,
-                drive_mode: DriveMode::PushPull,
-                duty_pct: 50,
-            })
-            .expect("should be able to configure ledc channel");
-
-        Self {
-            _ledc: ledc,
-            lstimer0,
-            channel0,
-        }
-    }
-
-    #[cfg(feature = "esp32s3")]
-    pub fn new(ledc_peripheral: LEDC<'static>, gpio_peripheral: GPIO34) -> Self {
-        static LEDC: StaticCell<Ledc<'static>> = StaticCell::new();
-        static LSTIMER0: StaticCell<Timer<'static, LowSpeed>> = StaticCell::new();
-        static CHANNEL0: StaticCell<Channel<'static, LowSpeed>> = StaticCell::new();
-
-        let ledc = LEDC.init(Ledc::new(ledc_peripheral));
-        ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
-
-        let lstimer0 = LSTIMER0.init(ledc.timer::<LowSpeed>(TimerNumber::Timer0));
-        lstimer0
-            .configure(LedcTimerConfig {
-                duty: Duty::Duty1Bit,
-                clock_source: LSClockSource::APBClk,
-                frequency: Rate::from_hz(1000),
-            })
-            .expect("should be able to configure ledc timer");
-
-        let lstimer0 = &(*lstimer0);
-
-        let channel0 = CHANNEL0.init(ledc.channel(ChannelNumber::Channel0, gpio_peripheral));
-        channel0
-            .configure(ChannelConfig {
-                timer: lstimer0,
-                drive_mode: DriveMode::PushPull,
-                duty_pct: 50,
-            })
-            .expect("should be able to configure ledc channel");
 
         Self {
             ledc,
             lstimer0,
-            channel0,
+            pin,
+            duty,
         }
     }
 }
@@ -101,18 +61,37 @@ impl SetFrequency for PwmController {
     fn set_divider(&mut self, divider: u32) {
         self.lstimer0.configure_hw(divider);
     }
-}
 
-impl ErrorType for PwmController {
-    type Error = channel::Error;
-}
-
-impl SetDutyCycle for PwmController {
-    fn max_duty_cycle(&self) -> u16 {
-        self.channel0.max_duty_cycle()
+    fn set_frequency_integer(&mut self, frequency: u32) {
+        // from <Timer as TimerIFace>::configure()
+        let src_freq: u32 = self
+            .lstimer0
+            .freq()
+            .expect("lstimer0 source frequency should be set")
+            .as_hz();
+        let precision = 1 << self.duty as u32;
+        let divider = ((src_freq as u64) << 8) / frequency as u64 / precision as u64;
+        self.set_divider(divider as u32);
     }
 
-    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
-        self.channel0.set_duty_cycle(duty)
+    async fn play_frequency_for_duration(
+        &mut self,
+        frequency: f32,
+        duration: embassy_time::Duration,
+    ) {
+        self.set_frequency_integer(frequency as u32);
+
+        let mut channel0 = self
+            .ledc
+            .channel(ChannelNumber::Channel0, self.pin.reborrow());
+        channel0
+            .configure(ChannelConfig {
+                timer: self.lstimer0,
+                drive_mode: DriveMode::PushPull,
+                duty_pct: 50,
+            })
+            .expect("should be able to configure ledc channel");
+
+        Timer::after(duration).await;
     }
 }
