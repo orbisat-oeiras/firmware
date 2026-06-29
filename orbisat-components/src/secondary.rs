1dd198f9de81aad417a48648b9cf7b1e91ea450f
Leviathan <levi.mcgomes@gmail.com>
2026-06-29 11:50:28 +0100

    feat(components): log the start timestamp of every sweep to a file

--- orbisat-components/src/secondary.rs
+++ orbisat-components/src/secondary.rs
@@ -1,17 +1,29 @@
 use embassy_time::{Duration, Timer};
-use orbipacket::DeviceId;
+use embedded_hal::spi::SpiDevice;
+use orbipacket::{DeviceId, TimestampError};
 use orbisat::{Component, comms::CommunicationError};
 
+use crate::sd::{SdError, SdFileWriter};
+
 #[derive(thiserror::Error)]
-pub enum SpeakerError {
+pub enum SpeakerError<SPI: SpiDevice<u8>> {
     #[error(transparent)]
     Communication(#[from] CommunicationError),
+    #[error(transparent)]
+    Sd(#[from] SdError<SPI>),
 }
 
-impl core::fmt::Debug for SpeakerError {
+impl<SPI: SpiDevice<u8>> From<TimestampError> for SpeakerError<SPI> {
+    fn from(value: TimestampError) -> Self {
+        Self::Communication(CommunicationError::Timestamp(value))
+    }
+}
+
+impl<SPI: SpiDevice<u8>> core::fmt::Debug for SpeakerError<SPI> {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         match self {
             SpeakerError::Communication(f0) => f.debug_tuple("Communication").field(&f0).finish(),
+            SpeakerError::Sd(sd_error) => f.debug_tuple("Sd").field(&sd_error).finish(),
         }
     }
 }
@@ -24,32 +36,38 @@ pub trait SetFrequency {
 
 type SweepData<'a> = &'a [(u32, u64)];
 
-#[derive(Debug)]
-pub struct SpeakerComponent<'a, PWM: SetFrequency> {
+pub struct SpeakerComponent<'a, PWM: SetFrequency, SPI: SpiDevice<u8>> {
     pwm: PWM,
     data: SweepData<'a>,
     forward_sweep: bool,
+    writer: SdFileWriter<'a, SPI>,
 }
 
-impl<'a, PWM: SetFrequency> SpeakerComponent<'a, PWM> {
-    pub fn new(pwm: PWM, data: SweepData<'a>) -> Self {
+impl<'a, PWM: SetFrequency, SPI: SpiDevice<u8>> SpeakerComponent<'a, PWM, SPI> {
+    pub fn new(pwm: PWM, data: SweepData<'a>, writer: SdFileWriter<'a, SPI>) -> Self {
         Self {
             pwm,
             data,
             forward_sweep: true,
+            writer,
         }
     }
 }
 
-impl<'a, PWM: SetFrequency> Component for SpeakerComponent<'a, PWM> {
-    type Error = SpeakerError;
+impl<'a, PWM: SetFrequency, SPI: SpiDevice<u8>> Component for SpeakerComponent<'a, PWM, SPI> {
+    type Error = SpeakerError<SPI>;
 
     fn id(&self) -> orbipacket::DeviceId {
         DeviceId::Mission1
     }
 
-    async fn run_once(&mut self, _ctx: &mut orbisat::ContextHandle<'_>) -> Result<(), Self::Error> {
+    async fn run_once(&mut self, ctx: &mut orbisat::ContextHandle<'_>) -> Result<(), Self::Error> {
         if self.forward_sweep {
+            self.writer.write(b"FORWARD SWEEP TIMESTAMP ", ctx).await?;
+            self.writer
+                .write(&ctx.timestamp()?.get().to_le_bytes(), ctx)
+                .await?;
+            self.writer.write(b"\n", ctx).await?;
             defmt::info!("Starting forward sweep");
             for (frequency, duration) in self.data {
                 self.pwm.set_frequency(*frequency).await;
@@ -57,6 +75,11 @@ impl<'a, PWM: SetFrequency> Component for SpeakerComponent<'a, PWM> {
             }
             self.forward_sweep = false;
         } else {
+            self.writer.write(b"REVERSE SWEEP TIMESTAMP ", ctx).await?;
+            self.writer
+                .write(&ctx.timestamp()?.get().to_le_bytes(), ctx)
+                .await?;
+            self.writer.write(b"\n", ctx).await?;
             defmt::info!("Starting reverse sweep");
             for (frequency, duration) in self.data.iter().rev() {
                 self.pwm.set_frequency(*frequency).await;
